d7b64a210102b44cdf8b6205403ea852b0deff3c
Chris Hebert <chrisdhebert@gmail.com>
2025-07-31 03:00:16 -0400

    Update sniper.sh

--- sniper.sh
+++ sniper.sh
@@ -261,12 +261,12 @@ echo "==========Phase 5 Eyewitness Web Thumbnail Scans ================"
 read -p "(?) Do you want to create thumbnails on ports (80,443,8000,8080,8443) with 'eyewitness' ?(y/N)" yn
 
 case $yn in
-	[Yy]* ) echo "(OK) Starting - Eyewitness Scan..."
+	[Yy]* ) echo "(OK) Starting - Eyewitness Scan...";
         	if test -f "./eyewitness/ew.db"; then echo "(OK) - Found unfinished scan - resuming.. ";
 			/usr/bin/sudo -u postgres $EYEWITNESS --resume ./eyewitness/ew.db ;
    			echo "(OK) eyewitness scan complete - see ./eyewitness/report.html";;
 		else
-			/bin/cp $CONF/msf_default.rc /tmp/sniper-eye.msf.rc;
+		    /bin/cp $CONF/msf_default.rc /tmp/sniper-eye.msf.rc;
 		    echo "services -p 80,443,8000,8080,8443 -u -o /tmp/sniper.eyewitness.txt"  >> /tmp/sniper-eye.msf.rc;
 		    echo "quit -y" >> /tmp/sniper-eye.msf.rc;
 		    $MSFBIN -r /tmp/sniper-eye.msf.rc;
